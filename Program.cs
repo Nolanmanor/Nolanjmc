7edfaf1f843d2f09a46dd4801bbe0d22170c481e
Nolan <nolanjmc707@gmail.com>
2020-02-25 13:36:21 -0800

    FizzBuzz in C#

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            
+        
+            int n = 101;
+
+            for (int i = 1; i < n; i++)
+            {
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                
+                
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                    
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                
+                else Console.WriteLine(i);
+
+
+            }
+
+
+
+
+            
+
+
+
+
+
+        }
+    }
+}
+
